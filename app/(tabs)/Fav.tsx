6eaee1f7d48ac6283d191bc9dcbe21c9bd4b56d1
GiOrGI175 <nozadzegiorgi1011@gmail.com>
2026-02-15 16:44:26 +0400

    feature/navigation rouht

---
+++ app/(tabs)/Fav.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const Fav = () => {
+  return (
+    <View>
+      <Text>Fav</Text>
+    </View>
+  );
+};
+
+export default Fav;
+
+const styles = StyleSheet.create({});
